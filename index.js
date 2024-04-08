8f738b7a7eeb5b3cb4752d06b639c9f9c141901c
foy4748 <faisaljfcl@gmail.com>
2024-04-09 03:07:27 +0600

    Interacting with Gemini | Done

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// Import zone
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
+
+// Gemini Related Package import
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// ===============================
 
 // Call dotenv to access env variables
 dotenv.config();
@@ -8,6 +15,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middlewares
+app.use(cors());            // For resolving CORS issues
+app.use(express.json());    // For parsing POST body as JS object
+
 // API Endpoints
 app.get("/", (req, res) => {
     res.send({
@@ -15,6 +26,31 @@ app.get("/", (req, res) => {
         message: "SERVER is UP and RUNNING"
     })
 })
+
+// Interacting with Gemini API
+app.post("/", async (req, res) => {
+    console.log(req.body);
+    const { prompt } = req.body
+    //========
+    // For text-only input, use the gemini-pro model
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_SECRET_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    //const prompt = "Write a story about a magic backpack."
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    //console.log(text);
+    //========
+
+    res.send({
+        error: false,
+        message: "POST successful",
+        gemini_response: text
+    })
+})
+// ------------------------------
 
 // Port Listening
 app.listen(PORT, () => console.log(`SERVER is UP and RUNNING at port ${PORT}\nLocal Address: http://localhost:${PORT}`))
